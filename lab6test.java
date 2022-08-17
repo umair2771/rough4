b38bd769455c2905ccf6785aca04d95c8aafc951
umair2771 <93678706+umair2771@users.noreply.github.com>
2022-08-17 06:24:14 -0400

    Add files via upload

--- lab6test.java
+++ lab6test.java
@@ -3,7 +3,7 @@ package lab6package;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
-import org.openqa.selenium.*;
+ 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,40 +11,68 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-import java.util.Iterator;
+ 
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.interactions.Action;
+ 
+ 
 import org.openqa.selenium.interactions.Actions;
 
 
 public class lab6test {
 	
-	String baseurl = "https://www.globalsqa.com/angularJs-protractor/BankingProject";
-	 public WebDriver driver;
-	 
+	String baseurl = "https://omayo.blogspot.in/";
+	public WebDriver driver;
+	public Actions actions;
+	public WebDriverWait wait;
+		 
 	 
   
   @BeforeTest
   public void beforeTest() {
-	  
-	  System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\4128\\\\Downloads/chromedriver.exe");
-	  WebDriver driver = new ChromeDriver();
-	  driver.get(baseurl);
-		
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\4128\\Downloads/chromedriver.exe");
+	  System.out.println("Launching Chrome browser..."); 
+	  WebDriver driver=new ChromeDriver();
+	  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20)); // wait object
+	  //Actions actions = new Actions(driver);
+	  driver.get(baseurl);    // loading Website link 
+	  WebElement textarea1= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[contains(text(),'Text Area Field')]//following::textarea[contains(@id,'ta')]"))); // wait for page to load
+	  driver.manage().window().maximize(); 
+	 
   }
-  @Test
-  public void managerlogin() {
+  
+  @Test(priority=1)
+  public void a() {
+	     WebElement textarea3= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[contains(text(),'Text Area Field')]//following::textarea[contains(@id,'ta')]")));
+	     //WebElement textarea1 = driver.findElement(By.xpath("//h2[contains(text(),'Text Area Field')]//following::textarea[contains(@id,'ta')]"));
+	     textarea3.sendKeys("Muhammad Umair");
+		 WebElement textarea2 = driver.findElement(By.xpath("//div[contains(@class,'widget-content')]//child::textarea[contains(@cols,'30') and contains(@rows,'10')]"));
+		 String text = textarea2.getText();
+		 System.out.println(text); // Printing the text field 2 text
+		 textarea2.clear();
+		 textarea2.sendKeys(" I like to Play Badminton ");
 	  
+  }
+  @Test(priority=2)
+  public void b() {
+	  //get tabel data
+	  String tabletext = null;
+		// List<WebElement> tableelements=(List<WebElement>)
+		// driver.findElement(By.xpath("//table[contains(@id,'table1')]"));
+		List<WebElement> allElements = driver.findElements(By.xpath("//table[contains(@id,'table1')]"));
+
+		for (WebElement element : allElements) {
+			tabletext = element.getText();
+			System.out.println("Table Text is " + tabletext);
+
+		}
 	  
 	  
   }
  
 
-  @AfterTest
-  public void afterTest() {
-	  driver.close();
-  }
+//  @AfterTest
+//  public void afterTest() {
+//	  driver.close();
+//  }
 
 }
